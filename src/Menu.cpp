2d66a2cbadf6ecfa7f0c01c4c32f6e43b9db77e4
Junier <onieldp@gmail.com>
2016-06-19 20:49:12 -0400

    Implementing the show method and adding the getAllBooks, addBooks, getLastBook, and getByCategory methods.

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -13,11 +13,99 @@ using namespace std;
 void Menu::show()
 {
 	//TODO: Implement this method.
-	cout << "Implement the menu" << endl;
+	int option;
+	do
+	{
+		clearScreen();
+		cout << "Library ITLA" << endl;
+		cout << "1- List books" << endl;
+		cout << "2- Add book" << endl;
+		cout << "3- Get the last book" << endl;
+		cout << "4- Get books by category" << endl;
+		cout << "5- Exit" << endl;
+		cout << "Select an option => ";
+		cin >> option;
+		if(validate(option))
+		{
+			routeAction(option);
+		}
+	}while(option != 5);
 }
 
 Menu::Menu()
 {
 
 }
+void Menu::clearScreen()
+{
+	#ifdef _WIN32
+	system("cls");
+	#else
+	system("clear");
+	#endif
+}
 
+void Menu::pause()
+{
+	#ifdef _WIN32
+	system("pause");
+	#else
+	system("read -n1 -r -p \"Press any key to continue...\" key");
+	#endif
+}
+
+void Menu::routeAction(int option)
+{
+	switch(option)
+	{
+	case 1:
+		getAllBooks();
+		break;
+	case 2:
+		addBook();
+		break;
+	case 3:
+		getLastBook();
+		break;
+	case 4:
+		getByCategory();
+		break;
+	case 5:
+		cout << "Thanks for using our software, good bye!" << endl;
+	}
+	pause();
+}
+
+bool Menu::validate(int option)
+{
+	if(option >= 1 && option <= 5)
+	{
+		return true;
+	}else
+	{
+		cout << "Please pick an option between 1 and 5"<< endl;
+		pause();
+		return false;
+	}
+}
+
+void Menu::getAllBooks()
+{
+	//TODO: Implement this
+}
+
+void Menu::addBook()
+{
+	//TODO: Implement this
+
+}
+
+void Menu::getLastBook()
+{
+	//TODO: Implement this
+}
+
+void Menu::getByCategory()
+{
+	//TODO: Implement this
+}
