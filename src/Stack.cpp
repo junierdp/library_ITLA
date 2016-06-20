0aff94816db694d5c30d25c6ccb25f4d17a0d478
Junier <onieldp@gmail.com>
2016-06-20 00:20:54 -0400

    Implementing the push method that add a new book int he stack.

--- src/Stack.cpp
+++ src/Stack.cpp
@@ -10,6 +10,15 @@
 void Stack::push(Element* element)
 {
 	//TODO: Implement this method.
+	if(_last == NULL)
+	{
+		_last = element;
+	}
+	else
+	{
+		element -> _next = _last;
+		_last = element;
+	}
 }
 
 Element* Stack::pop()
