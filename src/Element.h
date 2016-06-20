f8021854c758fa1c55eb49c8ac768c46b070bec7
Junier <onieldp@gmail.com>
2016-06-19 20:45:45 -0400

    Adding the privates atributes of an element and the public get methods that return the atributes of an element.

--- src/Element.h
+++ src/Element.h
@@ -11,11 +11,20 @@
 class Element
 {
 	private:
-		std::string _name;
+		std::string _title;
+		std::string _author;
+		std::string _publisher;
+		std::string _date;
+		std::string _category;
 		Element* _next;
 
 	public:
-		Element(std::string name);
+		Element(std::string title, std::string author, std::string publisher, std::string date, std::string category);
+		std::string getTitle(){return _title;}
+		std::string getAuthor(){return _author;}
+		std::string getPublisher(){return _publisher;}
+		std::string getDate(){return _date;}
+		std::string getCategory(){return _category;}
 	//This means that Stack can access directly to Element's private members
 	friend class Stack;
 };
